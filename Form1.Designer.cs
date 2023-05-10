33150bdfe45a5cbf62de5a19e286368c3ca3d77b
Anton <KimAnton3@yandex.ru>
2023-05-10 22:49:34 +0300

    Подключение всех компонентов и финальные правки программы

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.DaysSpisok = new System.Windows.Forms.ListBox();
             this.MaxSpeed_Input = new System.Windows.Forms.RichTextBox();
             this.SaveFile_Button = new System.Windows.Forms.Button();
@@ -207,6 +207,7 @@
             this.DrawGraphic.TabIndex = 17;
             this.DrawGraphic.Text = "Отобразить график";
             this.DrawGraphic.UseVisualStyleBackColor = true;
+            this.DrawGraphic.Click += new System.EventHandler(this.DrawGraphic_Click_1);
             // 
             // label6
             // 
@@ -228,16 +229,16 @@
             // 
             // chart1
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(509, 33);
             this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
             this.chart1.Size = new System.Drawing.Size(490, 230);
             this.chart1.TabIndex = 20;
             this.chart1.Text = "chart1";
--- Form1.cs
+++ Form1.cs
@@ -7,25 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Windows.Forms.DataVisualization.Charting;
 
 
 namespace ТП_Лаба_3
 {
     public partial class Form1 : Form
     {
-
-
         public List<Day> Days = new List<Day>();
-
+        const string path = @"C:\Users\User\Desktop\ФайлДляЛабы.txt";
+        public FileReader fr;
+        public GraphicDrawer gd;
+        public Statistics st;
+        public string[] parametres = new string[7] { "День года","Max Speed","Min Speed","Avarage speed"
+            ,"Duration(min)","Average pulse","Distance"};
         private void Form1_Load(object sender, EventArgs e)
         {
+            gd = new GraphicDrawer(chart1);
+            fr = new FileReader(path, this);
+            st = new Statistics();
+            fr.ReadFile();
+            UpdateStatistics();
+
         }
         public Form1()
         {
             InitializeComponent();
-        }
+            for (int i = 0; i < parametres.Length; i++)
+            {
+                ChooseType1.Items.Add(parametres[i]);
+                ChooseType2.Items.Add(parametres[i]);
+            }
 
+        }
         private void DaysSpisok_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (DaysSpisok.SelectedIndex != -1)
@@ -52,10 +66,90 @@ namespace ТП_Лаба_3
             temp.AveragePulse = float.Parse(AvaragePulse_Input.Text);
             temp.Distance = float.Parse(Distance_Input.Text);
             Days.Add(temp);
-            
+            UpdateStatistics();
             DaysSpisok.Items.Add(temp.date.ToString());
         }
-       
-       
+        private void SaveFile_Button_Click(object sender, EventArgs e)
+        {
+            fr.WriteFile();
+        }
+
+        double[] ChooseData(int parameter, out string type)
+        {
+            List<double> result = new List<double>();
+            type = "0";
+            switch (parameter)
+            {
+                case 0:
+                    for (int i = 0; i < Days.Count; i++)
+                    {
+                        result.Add(Days[i].date.DayOfYear);
+                        type = parametres[parameter];
+                    }
+                    break;
+                case 1:
+                    for (int i = 0; i < Days.Count; i++)
+                    {
+                        result.Add(Days[i].MaxSpeed);
+                        type = parametres[parameter];
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i < Days.Count; i++)
+                    {
+                        result.Add(Days[i].MinSpeed);
+                        type = parametres[parameter];
+                    }
+                    break;
+                case 3:
+                    for (int i = 0; i < Days.Count; i++)
+                    {
+                        result.Add(Days[i].Duration);
+                        type = parametres[parameter];
+                    }
+                    break;
+                case 4:
+                    for (int i = 0; i < Days.Count; i++)
+                    {
+                        result.Add(Days[i].AverageSpeed);
+                        type = parametres[parameter];
+                    }
+                    break;
+                case 5:
+                    for (int i = 0; i < Days.Count; i++)
+                    {
+                        result.Add(Days[i].AveragePulse);
+                        type = parametres[parameter];
+                    }
+                    break;
+                case 6:
+                    for (int i = 0; i < Days.Count; i++)
+                    {
+                        result.Add(Days[i].Distance);
+                        type = parametres[parameter];
+                    }
+                    break;
+            }
+            return result.ToArray();
+        }
+        public void UpdateStatistics()
+        {
+            float MaxSpeed;
+            float MaxTime;
+            float Distance;
+            st.UpdateStatistics(Days, out MaxSpeed, out MaxTime, out Distance);
+            MaxSpeed_st.Text = MaxSpeed.ToString();
+            MaxDuration_st.Text = MaxTime.ToString();
+            Distance_st.Text = Distance.ToString();
+        }
+
+        private void DrawGraphic_Click_1(object sender, EventArgs e)
+        {
+            string type1;
+            double[] data1 = ChooseData(ChooseType1.SelectedIndex, out type1);
+            string type2;
+            double[] data2 = ChooseData(ChooseType2.SelectedIndex, out type2);
+            gd.DrawGraphic(type1, type2, data1, data2);
+        }
     }
 }
