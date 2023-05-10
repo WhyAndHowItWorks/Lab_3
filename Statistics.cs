4e55a543dbb827792b334b31b6bd3cfaa6c536e8
Boris <borya.bikov@mail.ru>
2023-05-10 18:29:05 +0300

    Добавлен компонент для создания статистики по дням при помощи массива дней из формы. Добавлен компонент, который отрисовывает график по двум любым параметрам дня.

---
+++ Statistics.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ТП_Лаба_3
+{
+    public class Statistics
+    {
+        
+        public void UpdateStatistics(List<Day> days, out float MaxSpeed_res, out float MaxTime_res, out float Distance_res)
+        {
+            
+            //// Находим максимальную скорость
+            float res = 0;
+            for (int i = 0; i < days.Count; i++)
+            {
+                if (days[i].MaxSpeed > res)
+                {
+                    res = days[i].MaxSpeed;
+                }
+            }
+           MaxSpeed_res = res;
+            //// Находим максимальную длительность тренировки
+            res = 0;
+            for (int i = 0; i < days.Count; i++)
+            {
+                if (days[i].Duration > res)
+                {
+                    res = days[i].Duration;
+                }
+            }
+            MaxTime_res = res;
+            //// Находим колво км за выходные
+            res = 0;
+            for (int i = 0; i < days.Count; i++)
+            {
+                if (days[i].date.DayOfWeek == DayOfWeek.Sunday || days[i].date.DayOfWeek == DayOfWeek.Saturday)
+                {
+                    res += days[i].Distance;
+                }
+            }
+            Distance_res = res;
+
+        }
+
+
+    }
+}
