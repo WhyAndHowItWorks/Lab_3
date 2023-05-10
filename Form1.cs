4f91902b7e1e19a9ba004b2aa321dfd3d5989bd5
Aleksey <Ignatiev.Alex2015@yandex.ru>
2023-05-10 13:13:55 +0300

    Добавлено отображение данных дня из массива дней, если выбрать день в списке. Добавлена возможность добавления новых дней в массив.

--- Form1.cs
+++ Form1.cs
@@ -26,5 +26,36 @@ namespace ТП_Лаба_3
             InitializeComponent();
         }
 
+        private void DaysSpisok_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (DaysSpisok.SelectedIndex != -1)
+            {
+                Day temp = Days[DaysSpisok.SelectedIndex];
+                ChooseDate.Value = temp.date;
+                MaxSpeed_Input.Text = temp.MaxSpeed.ToString();
+                MinSpeed_Input.Text = temp.MinSpeed.ToString();
+                Duration_Input.Text = temp.Duration.ToString();
+                AvarageSpeed_Input.Text = temp.AverageSpeed.ToString();
+                AvaragePulse_Input.Text = temp.AveragePulse.ToString();
+                Distance_Input.Text = temp.Distance.ToString();
+            }
+        }
+
+        private void AddDay_Click(object sender, EventArgs e)
+        {
+            Day temp = new Day();
+            temp.date = ChooseDate.Value;
+            temp.MaxSpeed = float.Parse(MaxSpeed_Input.Text);
+            temp.MinSpeed = float.Parse(MinSpeed_Input.Text);
+            temp.Duration = Convert.ToInt32(Duration_Input.Text);
+            temp.AverageSpeed = float.Parse(AvarageSpeed_Input.Text);
+            temp.AveragePulse = float.Parse(AvaragePulse_Input.Text);
+            temp.Distance = float.Parse(Distance_Input.Text);
+            Days.Add(temp);
+            
+            DaysSpisok.Items.Add(temp.date.ToString());
+        }
+       
+       
     }
 }
